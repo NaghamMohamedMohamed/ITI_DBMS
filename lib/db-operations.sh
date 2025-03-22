62583301f5d180a0ea046fd902ce60fd0b10c27b
Nagham Mohamed <naghamfakhreldin@gmail.com>
2025-03-22 13:36:14 +0200

    feat(db_operations.sh): Adding new functions for connecting & droppinga db. , Creating global variable for storing the db path folder for optimized usage..

--- lib/db-operations.sh
+++ lib/db-operations.sh
@@ -1,9 +1,14 @@
-# This file is for DB Operations : Create , List , Connect , Drop
 #!/bin/bash
 
-create_db() {
-    DB_PATH="./Databases"
 
+source ./lib/table-menu.sh
+
+DB_PATH="./Databases"
+
+
+# Function to create a new database
+create_db() 
+{
     #check if the main database directory exists, if not, it is created
     if [[ ! -d $DB_PATH ]]; then
         mkdir -p "$DB_PATH"
@@ -13,41 +18,78 @@ create_db() {
         echo -e "Enter the name of the database or press 'q' to quit:"
         read dbname
 
-        #check if user wants to quit
+        # Check if user wants to quit
         if [[ $dbname == "q" ]]; then
             echo "Exiting database creation..."
             break
         fi
 
-        #check if the database already exists
+        # Check if the database already exists
         if [[ -d "$DB_PATH/$dbname" ]]; then
             echo "Database '$dbname' already exists!"
         else
             mkdir "$DB_PATH/$dbname"
-            echo "Database '$dbname' created successfully!"
+            echo "Database '$dbname' was created successfully!"
         fi
     done
 }
 
-list_db() {
 
-    DB_PATH="./Databases"
-    #checking if the databases directory exists
+# Function to list all databases
+list_db() 
+{
+    # Checking if the databases directory exists
     if [[ ! -d $DB_PATH ]]; then
         echo "Error: Database directory '$DB_PATH' not found!"
         return 1
     fi
 
-    #listing only directories
+    # Listing only directories
     databases=$(find "$DB_PATH" -mindepth 1 -maxdepth 1 -type d | sed "s|$DB_PATH/||")
-    #mindepth 1 : so it doesnt display the parent directory
-    #maxdepth 1 : so it doesnt display the subdirectories only child directories
-    #type d : only directories not files
-    #sed : to replace the path from the output with empty string
+    # mindepth 1 : so it doesnt display the parent directory
+    # maxdepth 1 : so it doesnt display the subdirectories only child directories
+    # type d : only directories not files
+    # sed : to replace the path from the output with empty string
     if [[ -z "$databases" ]]; then
         echo "No Databases Found!"
     else
-        echo "List of Databases:"
-        echo "$databases"
+        echo "List of Databases :"
+        echo "- $databases"
     fi
 }
+
+
+# Function to connect to a database (calls table operations menu)
+connect_to_db ()
+{
+    read -p "Enter the database name to connect : " db_name
+    if [[ -d "$DB_PATH/$db_name" ]]; then
+        echo
+        echo "Connected to database : $db_name"
+        export db_name
+
+        sleep 1
+        clear
+        sub_menu
+
+    else
+        echo
+        echo "Database does not exist!"
+    fi
+}
+
+
+# Function to drop/delete a database
+drop_db ()
+{
+    read -p "Enter database name to delete : " db_name
+    if [[ -d "$DB_PATH/$db_name" ]]; then
+        rm -r "$DB_PATH/$db_name"
+        echo
+        echo "Database '$db_name' was deleted successfully."
+    else
+        echo
+        echo "Database does not exist!"
+    fi
+}
+
