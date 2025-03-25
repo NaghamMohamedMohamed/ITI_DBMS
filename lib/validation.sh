6b51ab2ec99baebb3a01744b9f7b13bc83aad426
Nagham Mohamed <naghamfakhreldin@gmail.com>
2025-03-26 00:09:48 +0200

    feat(validation.sh): Adding validation for db naming constraints before existence checking.

--- lib/validation.sh
+++ lib/validation.sh
@@ -4,28 +4,30 @@
 # Function to validate DB before creation , connection , dropping.
 validate_db() 
 {
-    # A local variable to store the db name passedas an argument to this fucntion
+    # A local variable to store the db name passed as an argument to this fucntion
     local dbname="$1"
 
     # Validate database name
     if [[ ! "$dbname" =~ ^[a-zA-Z][a-zA-Z0-9_]*$ ]]; then
+        echo
         echo "Invalid database name. It must start with a letter and contain only letters, numbers, and underscores."
         echo
-        return 0
+        return 2
     fi
-
     
     # Convert db name to lowercase
     local dbname_lower=$(echo "$dbname" | tr '[:upper:]' '[:lower:]')
 
-    # Validate database pre-existence 
-    if [[ -d "$DB_PATH/$dbname_lower" ]]; then
-        # DB exists
-        echo "Database '$dbname' already exists!"
-        echo
-        return 0
-    fi
 
+    #loops through the existing dBs and convert each dB name to lowercase to compare with the entered name
+    for existing_db in "$DB_PATH"/*; do
+        existing_db_name=$(basename "$existing_db" | tr '[:upper:]' '[:lower:]')
+        if [[ "$existing_db_name" == "$dbname_lower" ]]; then
+            # The dB exists
+            return 0  
+        fi
+    done
+   
     return 1
 }
 
